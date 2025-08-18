7091af9494e655cb5fe8cb949031d6e8aa9d8f84
Saksham-Shukla-05 <get467997@gmail.com>
2025-08-18 21:49:54 +0530

    feat: implement routing and add main pages for the blog app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import LayoutPage from "./pages/Layout";
+import DashBoard from "./pages/DashBoard";
+import WriteArticle from "./pages/WriteArticle";
+import BlogTitles from "./pages/BlogTitles";
+import GenerateImages from "./pages/GenerateImages";
+import RemoveBg from "./pages/RemoveBackground";
+import RemoveObj from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
+import Community from "./pages/Community";
 
 function App() {
-  return <div className=" ">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/ai" element={<LayoutPage />}>
+          <Route index element={<DashBoard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="remove-bg" element={<RemoveBg />} />
+          <Route path="remove-obj" element={<RemoveObj />} />
+          <Route path="resume-review" element={<ReviewResume />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="community" element={<Community />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
